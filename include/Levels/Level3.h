e53ecd2d87f2841454ee16f848ba2f5c0fbb86e2
dn9uyen <dannynguyen84268@gmail.com>
2023-12-03 17:10:18 -0800

    Add Levels 3-5

--- include/Levels/Level3.h
+++ include/Levels/Level3.h
@@ -9,26 +9,39 @@
 class Level3 : public Level {
   public:
     Level3() {
-        playerBallCount = 5;
+        playerBallCount = 6;
 
         playerBall.setColor({0.65f, 0.65f, 0.65f});
 
         // Non mandatory
-        entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {-0.7f, 0.5f}));
-        entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {-0.3f, 0.6f}));
+        for (int i = 0; i < 15; i++) {
+            entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {-0.9f + 0.05f * i, 0.7f - 0.1f * i}));
+            entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {0.9f - 0.05f * i, 0.7f - 0.1f * i}));
+        }
+
+        entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {0.0f, 0.0f}));
+        entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {-0.5f, 0.5f}));
+        entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {0.5f, 0.5f}));
+        entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {-0.3f, 0.5f}));
+        entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {0.3f, 0.5f}));
+        entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {-0.2f, 0.3f}));
+        entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {0.2f, 0.3f}));
+        entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {-0.15f, 0.15f}));
+        entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {0.15f, 0.15f}));
 
         // Mandatory
-        entities.push_back(new Ball(0.05, {1.0f, 0.5, 0.0f}, {-0.7f, 0.0f}));
-        dynamic_cast<Ball*>(entities.back())->setIsMandatory(true);
-        entities.push_back(new Ball(0.05, {1.0f, 0.5, 0.0f}, {-0.3f, 0.2f}));
-        dynamic_cast<Ball*>(entities.back())->setIsMandatory(true);
-        entities.push_back(new Ball(0.05, {1.0f, 0.5, 0.0f}, {0.1f, 0.0f}));
-        dynamic_cast<Ball*>(entities.back())->setIsMandatory(true);
-        entities.push_back(new Ball(0.05, {1.0f, 0.5, 0.0f}, {0.5f, 0.2f}));
-        dynamic_cast<Ball*>(entities.back())->setIsMandatory(true);
-        entities.push_back(new Ball(0.05, {1.0f, 0.5, 0.0f}, {0.9f, 0.0f}));
-        dynamic_cast<Ball*>(entities.back())->setIsMandatory(true);
-
+        for (int i = 0; i < 5; i++) {
+            entities.push_back(new Ball(0.05, {1.0f, 0.5, 0.0f}, {-0.95f + 0.05f * i, 0.4f - 0.3f * i}));
+            dynamic_cast<Ball *>(entities.back())->setIsMandatory(true);
+            entities.push_back(new Ball(0.05, {1.0f, 0.5, 0.0f}, {0.95f - 0.05f * i, 0.4f - 0.3f * i}));
+            dynamic_cast<Ball *>(entities.back())->setIsMandatory(true);
+        }
+        for (int i = 0; i < 5; i++) {
+            entities.push_back(new Ball(0.05, {1.0f, 0.5, 0.0f}, {-0.4f + 0.05f * i, 0.4f - 0.2f * i}));
+            dynamic_cast<Ball *>(entities.back())->setIsMandatory(true);
+            entities.push_back(new Ball(0.05, {1.0f, 0.5, 0.0f}, {0.4f - 0.05f * i, 0.4f - 0.2f * i}));
+            dynamic_cast<Ball *>(entities.back())->setIsMandatory(true);
+        }
     }
 };
 
--- include/Levels/Level5.h
+++ include/Levels/Level5.h
@@ -5,32 +5,27 @@
 #include "../Level.h"
 #include "../PlayerBall.h"
 #include <QtCore/qdebug.h>
+#include <random>
 
 class Level5 : public Level {
   public:
     Level5() {
+
         playerBallCount = 5;
 
         playerBall.setColor({0.65f, 0.65f, 0.65f});
 
-        // Non mandatory
-        entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {-0.7f, 0.5f}));
-        entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {-0.3f, 0.6f}));
-        entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {0.1f, 0.5f}));
-        entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {0.5f, 0.4f}));
-
-        // Mandatory
-        entities.push_back(new Ball(0.05, {1.0f, 0.5, 0.0f}, {-0.7f, 0.0f}));
-        dynamic_cast<Ball*>(entities.back())->setIsMandatory(true);
-        entities.push_back(new Ball(0.05, {1.0f, 0.5, 0.0f}, {-0.3f, 0.2f}));
-        dynamic_cast<Ball*>(entities.back())->setIsMandatory(true);
-        entities.push_back(new Ball(0.05, {1.0f, 0.5, 0.0f}, {0.1f, 0.0f}));
-        dynamic_cast<Ball*>(entities.back())->setIsMandatory(true);
-        entities.push_back(new Ball(0.05, {1.0f, 0.5, 0.0f}, {0.5f, 0.2f}));
-        dynamic_cast<Ball*>(entities.back())->setIsMandatory(true);
-        entities.push_back(new Ball(0.05, {1.0f, 0.5, 0.0f}, {0.9f, 0.0f}));
-        dynamic_cast<Ball*>(entities.back())->setIsMandatory(true);
-
+        auto gen = std::default_random_engine(time(0));
+        auto rand = std::uniform_real_distribution<float>(-0.9f, 0.9f);
+        for (int i = 0; i < 20; i++) {
+            // Non mandatory
+            entities.push_back(new Ball(0.05, {0.1f, 0.5, 0.7f}, {rand(gen), rand(gen) / 1.5f}));
+        }
+        for (int i = 0; i < 10; i++) {
+            // Non mandatory
+            entities.push_back(new Ball(0.05, {1.0f, 0.5, 0.0f}, {rand(gen), rand(gen) / 1.5f}));
+            dynamic_cast<Ball *>(entities.back())->setIsMandatory(true);
+        }
     }
 };
 
